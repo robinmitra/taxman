53969654958e906e5655a2285152637dcb1f48ec
Robin Mitra <robinmitra1@gmail.com>
2018-09-26 18:23:49 +0100

    Add the ability to get either monthly or yearly salary for an instance of `Employment`.

--- income/employment.py
+++ income/employment.py
@@ -1,6 +1,6 @@
 class Employment:
-    def __init__(self, salary):
-        self._salary = salary
+    def __init__(self, salary, period='yearly'):
+        self._salary = salary if period == 'yearly' else salary * 12
 
-    def get_salary(self):
-        return self._salary
+    def get_salary(self, period='yearly'):
+        return self._salary if period == 'yearly' else self._salary / 12
