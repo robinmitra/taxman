210f62d62cd4e1c04eabc718556612300b0c2087
Robin Mitra <robinmitra1@gmail.com>
2018-11-04 23:47:43 +0000

    Add more details to `setup.py`, include README and LICENSE in distribution, and disable output capturing when running unit tests for better debugging.

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+# Disable capturing of output sent to `stdout` and `stderr` when running
+# tests using Pytest.
+PYTEST_ARGS := -s
+
 # Run all tests.
 .PHONY: test
 test: test-flake8 test-unit
@@ -10,4 +14,4 @@ test-flake8:
 # Run unit tests.
 .PHONY: test-unit
 test-unit:
-	py.test
+	py.test ${PYTEST_ARGS}
--- setup.py
+++ setup.py
@@ -1,17 +1,37 @@
 from distutils.core import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='taxman',
     version='0.1.0',
-    packages=['taxman', 'taxman.allowance', 'taxman.calculator', 'taxman.income',
-              'taxman.national_insurance', 'taxman.person', 'taxman.tax'],
+    packages=['taxman', 'taxman.allowance', 'taxman.calculator',
+              'taxman.income', 'taxman.national_insurance', 'taxman.person',
+              'taxman.tax'],
     scripts=['bin/taxman'],
 
     # Metadata
     author='Robin Mitra',
-    author_email='robinmitra1@gmail.com',
-    description='Tax calculator',
+    description='A set of tools for computing Income Tax and National '
+                'Insurance contributions for the UK.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/robinmitra/taxman',
     license='MIT',
-    keywords='tax, calculator'
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Education',
+        'Topic :: Office/Business :: Financial :: Accounting',
+    ],
+    keywords='tax ni calculator taxman uk',
 )
